56f77eb17026d245e654e999d87a40a8fa873b61
amirhpooladi <104260321+amirhpooladi@users.noreply.github.com>
2022-07-13 16:59:03 +0430

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+
+function risheHa (a,b,c){
+
+var delta=(b*b)-(4*a*c);
+var jazrD=Math.sqrt(delta);
+x1=(-b+jazrD)/(2*a);
+x2=(-b-jazrD)/(2*a);
+}
+
+ function handleClick(){
+    var a1=document.getElementById("a").value;
+    var b1=document.getElementById("b").value;
+    var c1=document.getElementById("c").value;
+    risheHa(a1,b1,c1);
+
+
+  
+ 
+    
+ 
+    document.getElementById("x1").innerHTML=x1;
+    document.getElementById("x2").innerHTML=x2;
+ 
+   
+ }
+
+
+
+
+
